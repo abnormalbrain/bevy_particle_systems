2af1a9a0926524b4705729cdccf21900ea092bb0
Larry Du <larry.du@freenome.com>
2022-10-27 13:03:50 -0700

    Fixed documentation refs.

--- src/values.rs
+++ src/values.rs
@@ -243,7 +243,7 @@ impl Gradient {
     ///
     /// ``pct`` will be clamped between 0.0 and 1.0.
     ///
-    /// Returns [`bevy_render::prelude::Color::FUCHSIA`] as a fallback if no color is found for ``pct``. This indicates
+    /// Returns [`bevy::prelude::Color::FUCHSIA`] as a fallback if no color is found for ``pct``. This indicates
     /// that the gradient is misconfigured.
     pub fn get_color(&self, pct: f32) -> Color {
         let clamped_pct = pct.clamp(0.0, 1.0);
