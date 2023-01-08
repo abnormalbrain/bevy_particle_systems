bc99d6cf10fbbe7ad9a79dcfa239678b9c4e0517
Abnormal Brain Studios <102993888+abnormalbrain@users.noreply.github.com>
2023-01-07 20:08:30 -0500

    Fix particle color gradients when using texture atlas particles (#30)

--- src/plugin.rs
+++
@@ -1,9 +0,0 @@
-use bevy::prelude::Plugin;
-
-use crate::{
-    systems::{
-        partcle_spawner, particle_cleanup, particle_color, particle_lifetime, particle_transform,
-    },
-    TimeScale,
-};
-
